278d7802cede6e0893f8813610bee983b5b96777
Aryaman Paliwal <83011117+aryamanpaliwal@users.noreply.github.com>
2022-12-21 18:52:26 +0530

    Updated Solution - GfG to GitHub

--- Medium/0 - 1 Knapsack Problem/0-1-knapsack-problem.cpp
+++ Medium/0 - 1 Knapsack Problem/0-1-knapsack-problem.cpp
@@ -7,51 +7,32 @@ using namespace std;
 class Solution
 {
     public:
-    int knapSackRec(int W, int wt[], int val[], int i, int** dp)
-{
-    // base condition
-    if (i < 0)
-        return 0;
-    if (dp[i][W] != -1)
-        return dp[i][W];
- 
-    if (wt[i] > W) {
- 
-        // Store the value of function call
-        // stack in table before return
-        dp[i][W] = knapSackRec(W, wt, val, i - 1, dp);
-        return dp[i][W];
+    int knapSack(int w, int wt[], int val[], int n) 
+    {
+        int dp[n+1][w+1];
+        for(int i=0;i<n+1;i++)
+        {
+            for(int j=0;j<w+1;j++)
+            {
+                if(i==0 || j==0)
+                {
+                    dp[i][j]=0;
+                }
+            }
+        }
+        
+        for(int i=1;i<=n;i++)
+        {
+            for(int j=1;j<=w;j++)
+            {
+                if((j-wt[i-1])>=0)
+                    dp[i][j]=max(dp[i-1][j], val[i-1] + dp[i-1][j-wt[i-1]]);
+                else
+                    dp[i][j]=dp[i-1][j];
+            }
+        }
+        return dp[n][w];
     }
-    else {
-        // Store value in a table before return
-        dp[i][W] = max(val[i]
-                           + knapSackRec(W - wt[i], wt, val,
-                                         i - 1, dp),
-                       knapSackRec(W, wt, val, i - 1, dp));
- 
-        // Return value of table after storing
-        return dp[i][W];
-    }
-}
- 
-int knapSack(int W, int wt[], int val[], int n)
-{
-    // double pointer to declare the
-    // table dynamically
-    int** dp;
-    dp = new int*[n];
- 
-    // loop to create the table dynamically
-    for (int i = 0; i < n; i++)
-        dp[i] = new int[W + 1];
- 
-    // loop to initially filled the
-    // table with -1
-    for (int i = 0; i < n; i++)
-        for (int j = 0; j < W + 1; j++)
-            dp[i][j] = -1;
-    return knapSackRec(W, wt, val, n - 1, dp);
-}
 };
 
 //{ Driver Code Starts.
