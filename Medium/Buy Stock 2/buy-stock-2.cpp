892b990c563b4d9739ae09997e01ac1189fc1d51
Aryaman Paliwal <83011117+aryamanpaliwal@users.noreply.github.com>
2023-01-01 23:41:53 +0530

    Added Solution - GfG to GitHub

---
+++ Medium/Buy Stock 2/buy-stock-2.cpp
@@ -0,0 +1,41 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+class Solution{
+    public:
+    long long maximumProfit(vector<long long>&a, int n) {
+        // Code here
+        long long res=0;
+        for(int i=1;i<n;i++)
+        {
+            if(a[i]>a[i-1])
+            {
+                res+=(a[i]-a[i-1]);
+            }
+        }
+        return res;
+    }
+};
+
+//{ Driver Code Starts.
+int main()
+{
+    int t;
+    cin>>t;
+    while(t--){
+        int n; cin>>n;
+        vector<long long> prices;
+        
+        for(int i=0; i<n; ++i){
+            long long x; cin>>x;
+            prices.push_back(x);
+        }
+        
+        Solution obj;
+        cout<<obj.maximumProfit(prices, n)<<"\n";
+    }
+    return 0;
+}
+// } Driver Code Ends
